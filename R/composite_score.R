780c7f4c9c6251dc44a5a7294aac24e5df45d950
imh-ds <147938744+imh-ds@users.noreply.github.com>
2023-11-26 00:36:22 -0800

    Description update

--- R/composite_score.R
+++ R/composite_score.R
@@ -33,12 +33,12 @@
 #'                 openness = sprintf("o%01d", seq(10)),
 #'                 grit = sprintf("gs%01d", seq(12)))
 #' 
-#' # Calculate correlation weighted composite scores
+#' # Calculate correlation-weighted composite scores
 #' correlation_data <- composite_score(data = dat,
 #'                                     varlist = varlist,
 #'                                     weight = "correlation")
 #' 
-#' # Calculate regression weighted composite scores
+#' # Calculate regression-weighted composite scores
 #' regression_data <- composite_score(data = dat,
 #'                                    varlist = varlist,
 #'                                    weight = "regression")
